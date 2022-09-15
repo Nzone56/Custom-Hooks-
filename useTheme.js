21ef8017af7f014e00c0e0a1fb93451ef0e7bdbe
nzone56 <jpereirap@unal.edu.co>
2022-09-14 20:15:56 -0500

    Primer Commit

---
+++ useTheme.js
@@ -0,0 +1,15 @@
+// import { useReducer } from "react"
+// import { themeReducer } from "../08-useReducer/themeReducer"
+
+// const initialState = [
+//   {
+//     theme: "light",
+//     payload: "#000000"
+//   },
+// ];
+
+// export const useTheme = () => {
+
+//     const [theme, dispatch] = useReducer(themeReducer, initialState)
+
+// }
---
+++ useTodo.js
@@ -0,0 +1,54 @@
+import { useEffect, useReducer } from "react";
+import { todoReducer } from "../08-useReducer/todoReducer";
+
+const initialState = [ ];
+
+const init = () => {
+  return JSON.parse(localStorage.getItem("todos")) || [];
+};
+
+
+export const useTodo = () => {
+
+    const [todos, dispatchTodo] = useReducer(todoReducer, initialState, init);
+
+    useEffect(() => {
+      localStorage.setItem("todos", JSON.stringify(todos));
+    }, [todos]);
+
+    const handleNewTodo = (todo) => {
+      const action = {
+        type: "[TODO] Add Todo ",
+        payload: todo,
+      };
+
+      dispatchTodo( action );
+    };
+
+    const handleDeleteTodo = (id) => {
+      dispatchTodo({
+        type: "[TODO] Remove Todo",
+        payload: id,
+      });
+    };
+
+    const handleToggleTodo = (id) => {
+      dispatchTodo({
+        type: "[TODO] Toggle Todo",
+        payload: id,
+      });
+    };
+
+    const allTodos = todos.length;
+    const pendingTodosCount = (todos.filter( todo => !todo.done )).length;
+
+    return{
+         todos,
+         handleDeleteTodo,
+         handleNewTodo,
+         handleToggleTodo,
+         allTodos,
+         pendingTodosCount
+    }
+ 
+}
